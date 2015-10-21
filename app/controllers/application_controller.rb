cf16726cc6b27bebfd5caac5d05c3e00480b1702
David Kong <david.siqi.kong@gmail.com>
2015-10-21 16:10:14 -0700

    change profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :banned
+
+    def banned
+        if current_user && current_user.role == 'banned'
+            redirect_to '/banned'
+        end
+    end
 end
