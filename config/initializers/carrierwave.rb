4b2180753cc021c8591fb7024e9279fb0ac9c271
David Kong <david.siqi.kong@gmail.com>
2015-12-16 00:29:50 -0800

    changes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,6 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],                        # required
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],                       # required
-    region: ENV['AWS_REGION'],
     path_style: true
 
   }
