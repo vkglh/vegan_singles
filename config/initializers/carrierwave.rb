8da4caa501935171525618dafbd0a469fc766791
David Kong <david.siqi.kong@gmail.com>
2015-12-16 16:39:03 -0800

    add region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],                        # required
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],                       # required
+    region: ENV["AWS_REGION"]
   }
   config.fog_directory  = ENV["AWS_BUCKET"]                          # required
   config.fog_public     = true                                       # optional, defaults to true
